a1b6b0712c72c5de2fa6bf2c78de1b6d043dc9e1
Haris Khan <haris90400@gmail.com>
2024-03-15 01:43:19 +0530

    Also added bloc layer in the presentation layer,connection is pending

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:blog_app/core/secrets/app_secret.dart';
 import 'package:blog_app/core/theme/theme.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -12,7 +17,20 @@ void main() async {
     url: supabaseUrl!,
     anonKey: supabaseApiKey!,
   );
-  runApp(const MyApp());
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (_) => AuthBloc(
+          userSignUp: UserSignUp(
+            authRepository: AuthRepositoryImpl(
+              AuthRemoteDataSourceImpl(supabaseClient: supabase.client),
+            ),
+          ),
+        ),
+      ),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
