c94d5320cea2cd00d0dad4f68e289a59690ccff6
Haris Khan <haris90400@gmail.com>
2024-03-16 15:00:06 +0530

    Completed with dependency injection using get_it package

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,18 @@
-import 'package:blog_app/core/secrets/app_secret.dart';
 import 'package:blog_app/core/theme/theme.dart';
-import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
-import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
+import 'package:blog_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
-  final supabase = await Supabase.initialize(
-    url: supabaseUrl!,
-    anonKey: supabaseApiKey!,
-  );
+  await initDependencies();
+
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(
-        create: (_) => AuthBloc(
-          userSignUp: UserSignUp(
-            authRepository: AuthRepositoryImpl(
-              AuthRemoteDataSourceImpl(
-                supabaseClient: supabase.client,
-              ),
-            ),
-          ),
-        ),
+        create: (_) => serviceLocator<AuthBloc>(),
       ),
     ],
     child: const MyApp(),
